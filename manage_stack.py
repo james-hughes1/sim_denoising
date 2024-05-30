59fa2ae54fe515ee884dad45b3bbfa87c75a0ec8
James Hughes <jh2284@cam.ac.uk>
2024-05-30 08:33:13 +0100

    Fix: manage_stack properly processes 3D data in paz form, and allows partial directory processing.

--- manage_stack.py
+++ manage_stack.py
@@ -15,12 +15,19 @@ parser.add_argument(
 parser.add_argument("-q", "--num_acquisions", type=int, default=15)
 parser.add_argument("-g", "--glob_str", default="*.tif")
 parser.add_argument("-u", "--unstack", action="store_true")
+parser.add_argument("-s", "--start_index", type=int, default=0)
+parser.add_argument("-e", "--end_index", type=int, default=-1)
 args = parser.parse_args()
 
 output_dir = pathlib.Path(args.output_dir)
 output_dir.mkdir(parents=True, exist_ok=True)
 
 files = sorted(list(pathlib.Path(args.input_dir).glob(args.glob_str)))
+
+if args.end_index != -1:
+    files = files[args.start_index : args.end_index]
+else:
+    files = files[args.start_index :]
 
 if not args.unstack:
     if args.dimension == 2:
@@ -37,14 +44,18 @@ if not args.unstack:
     else:
         # Expect paz format
         n_acq = args.num_acquisions
+        n_z = sample.shape[0] // n_acq
         sample = tifffile.imread(files[0])
-        stack = np.zeros(
-            (len(files) * sample.shape[0] // n_acq, n_acq, *sample.shape[1:])
-        ).astype(sample.dtype)
+        stack = np.zeros((len(files) * n_z, n_acq, *sample.shape[1:])).astype(
+            sample.dtype
+        )
         for i, input_file in enumerate(files):
             print("\nProcessing", input_file.name)
             img_data = tifffile.imread(input_file)
-            stack[i, ...] = img_data
+            for z in range(n_z):
+                stack[i * n_z + z, ...] = img_data[
+                    z * n_acq : (z + 1) * n_acq, ...
+                ]
 
         output_file = output_dir / args.output_name
         tifffile.imwrite(str(output_file) + ".tif", stack)
