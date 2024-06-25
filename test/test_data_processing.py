9aca785c2cb50b8519e1bf0e479f1d5984358f9f
James Hughes <jh2284@cam.ac.uk>
2024-06-25 16:29:29 +0100

    Test: Further unit tests and error catching.

--- test/test_data_processing.py
+++ test/test_data_processing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from src.rcan.data_processing import (
     crop_volume,
@@ -6,12 +7,13 @@ from src.rcan.data_processing import (
     conv_czxy_to_omx,
     conv_omx_to_paz,
     conv_paz_to_omx,
+    ImageStack,
 )
 
 VOLUME = np.arange(3 * 100 * 80).reshape((3, 100, 80))
 
 
-def test_crop_volume():
+def test_crop_volume_valid():
     crop_volume_generator = crop_volume(
         VOLUME, num_steps=(3, 3), start=(70, 10), step=(5, 10), label="volume"
     )
@@ -22,30 +24,72 @@ def test_crop_volume():
             assert filename == "volume_008.tif"
 
 
+def test_crop_volume_invalid():
+    with pytest.raises(ValueError) as exc_info:
+        crop_volume_generator = crop_volume(
+            VOLUME,
+            num_steps=(3, 3),
+            start=(70, 10),
+            step=(5, 500),
+            label="volume",
+        )
+        for array, filename in crop_volume_generator:
+            pass
+    assert (
+        str(exc_info.value)
+        == "Cropping layout out of bounds in lateral dimension 1."
+    )
+
+
 IMAGE_CZXY = np.arange(15 * 3 * 2 * 2).reshape((15, 3, 2, 2))
 IMAGE_OMX = np.arange(45 * 2 * 2).reshape((45, 2, 2))
 IMAGE_PAZ = np.arange(45 * 2 * 2).reshape((45, 2, 2))
 
 
+# Test that in both images, 4th phase, 1st angle, 2nd z-plane image match up
 def test_conv_omx_to_czxy():
     image_czxy = conv_omx_to_czxy(IMAGE_OMX, 5, 3)
-    # 4th phase, 1st angle, 2nd z-plane
     assert (image_czxy[3, 1] == IMAGE_OMX[8]).all()
 
 
 def test_conv_czxy_to_omx():
     image_omx = conv_czxy_to_omx(IMAGE_CZXY, 5, 3)
-    # 4th phase, 1st angle, 2nd z-plane
     assert (image_omx[8] == IMAGE_CZXY[3, 1]).all()
 
 
 def test_conv_omx_to_paz():
     image_paz = conv_omx_to_paz(IMAGE_OMX, 5, 3)
-    # 4th phase, 1st angle, 2nd z-plane
     assert (image_paz[18] == IMAGE_OMX[8]).all()
 
 
 def test_conv_paz_to_omx():
     image_omx = conv_paz_to_omx(IMAGE_PAZ, 5, 3)
-    # 4th phase, 1st angle, 2nd z-plane
     assert (image_omx[8] == IMAGE_PAZ[18]).all()
+
+
+# Simulate 2 3D SIM stacks 3x2x2 size.
+IMAGES = [np.arange(15 * 3 * 2 * 2).reshape((45, 2, 2)) for _ in range(2)]
+
+
+def test_image_stack_valid():
+    # Test the remainder image stack size is computed correctly.
+    image_stack = ImageStack(3, 3, 2, IMAGES[0], range(8), 15)
+    image_stack.add_image(IMAGES[1], 1)
+    stack = image_stack.export_stack()
+    assert stack.shape == (6, 15, 2, 2)
+    assert (stack[:3] == 0).all()
+    assert (stack[4, ...] == IMAGES[1][15:30, ...]).all()
+
+
+def test_image_stack_invalid_shape():
+    image_stack = ImageStack(3, 3, 2, IMAGES[0], range(8), 15)
+    with pytest.raises(ValueError) as exc_info:
+        image_stack.add_image(np.ones((1, 1)), 1)
+    assert str(exc_info.value) == "all images must be the same shape."
+
+
+def test_image_stack_invalid_type():
+    image_stack = ImageStack(3, 3, 2, IMAGES[0], range(8), 15)
+    with pytest.raises(ValueError) as exc_info:
+        image_stack.add_image(np.ones((45, 2, 2), dtype="uint8"), 1)
+    assert str(exc_info.value) == "all images must have the same data type."
