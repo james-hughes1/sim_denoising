672d6c2d187881715679e4c5fa74133e875ada19
James Hughes <jh2284@cam.ac.uk>
2024-06-25 16:29:08 +0100

    Refactor: Improve arguments and modularity for manage_stack.py.

--- src/manage_stack.py
+++ src/manage_stack.py
@@ -4,26 +4,30 @@ the purpose of batch reconstructions.
 
 @details Takes a directory of images as input, and either stacks or unstacks
 the images there according to the configuration. 3D Image Volumes are expected
-to be in PAZ format.
+to be in PAZ format. Note in unstack mode, images are saved with a first
+dimension of length 1 - this is the correct format for training the second
+step models (CZXY).
 
 Arguments:
 - i: directory path of input images
 - o: directory path of output images
 - n: output image name prefix - only applies in 'stack' mode
 - d: dimension
-- q: number of SIM acquisitions per image - currently also used to set the
-number of z-planes per image when unstacking reconstructions
+- q: number of SIM acquisitions per image
 - g: glob string used to choose images from input directory
 - u: if used, sets mode to 'unstack'
 - s: start index of sorted input files to process
 - e: end index of sorted input files to process
-- t: number of images to stack together - only applies in 'stack' mode
+- t: number of images to stack together - only applies in 'stack' mode.
+Default: -1 (all images are stacked)
+- z: number of z slices of images - only applies in 'unstack' mode
 """
 
 import tifffile
-import numpy as np
 import argparse
 import pathlib
+
+from rcan.utils.data_processing import ImageStack
 
 
 # Parse arguments
@@ -40,11 +44,13 @@ parser.add_argument("-u", "--unstack", action="store_true")
 parser.add_argument("-s", "--start_index", type=int, default=0)
 parser.add_argument("-e", "--end_index", type=int, default=-1)
 parser.add_argument("-t", "--stack_number", type=int, default=-1)
+parser.add_argument("-z", "--z_slices", type=int, default=1)
 args = parser.parse_args()
 
 output_dir = pathlib.Path(args.output_dir)
 output_dir.mkdir(parents=True, exist_ok=True)
 
+# Sort directory files and select subset
 files = sorted(list(pathlib.Path(args.input_dir).glob(args.glob_str)))
 
 if args.end_index != -1:
@@ -53,92 +59,51 @@ else:
     files = files[args.start_index :]
 
 if not args.unstack:
+    # Stacking procedure
     stack_number = len(files) if args.stack_number == -1 else args.stack_number
+
+    # Compute number of stacks; if number of files isn't a multiple of stack
+    # number, this is no. files // stack_number + 1 for the remainder.
     number_of_stacks = len(files) // stack_number
     if len(files) % stack_number != 0:
         number_of_stacks += 1
+
     for stack_idx in range(number_of_stacks):
-        if args.dimension == 2:
-            sample = tifffile.imread(files[0])
-            stack = np.zeros(
-                (
-                    len(
-                        files[
-                            stack_idx
-                            * stack_number : (stack_idx + 1)
-                            * stack_number
-                        ]
-                    ),
-                    *sample.shape,
-                )
-            ).astype(sample.dtype)
-
-            for i, input_file in enumerate(
-                files[
-                    stack_idx * stack_number : (stack_idx + 1) * stack_number
-                ]
-            ):
-                print("\nProcessing", input_file.name)
-                img_data = tifffile.imread(input_file)
-                stack[i, ...] = img_data
-
-            filename = (
-                args.output_name
-                + f"_stack{stack_idx*stack_number:04d}"
-                + f"_{(stack_idx+1)*stack_number:04d}"
-            )
-            output_file = output_dir / filename
-            tifffile.imwrite(str(output_file) + ".tif", stack)
-        else:
-            # Expect paz format
-            n_acq = args.num_acquisitions
-            sample = tifffile.imread(files[0])
-            n_z = sample.shape[0] // n_acq
-            stack = np.zeros(
-                (
-                    len(
-                        files[
-                            stack_idx
-                            * stack_number : (stack_idx + 1)
-                            * stack_number
-                        ]
-                    )
-                    * n_z,
-                    n_acq,
-                    *sample.shape[1:],
-                )
-            ).astype(sample.dtype)
-            for i, input_file in enumerate(
-                files[
-                    stack_idx * stack_number : (stack_idx + 1) * stack_number
-                ]
-            ):
-                print("\nProcessing", input_file.name)
-                img_data = tifffile.imread(input_file)
-                for z in range(n_z):
-                    stack[i * n_z + z, ...] = img_data[
-                        z * n_acq : (z + 1) * n_acq, ...
-                    ]
-
-            filename = (
-                args.output_name
-                + f"_stack{stack_idx*stack_number:04d}"
-                + f"_{(stack_idx+1)*stack_number:04d}"
-            )
-            output_file = output_dir / filename
-            tifffile.imwrite(str(output_file) + ".tif", stack)
+        sample = tifffile.imread(files[0])
+        stack_handler = ImageStack(
+            args.dimension,
+            stack_number,
+            stack_idx,
+            sample,
+            files,
+            args.num_acquisitions,
+        )
+        for i, input_file in enumerate(
+            files[stack_idx * stack_number : (stack_idx + 1) * stack_number]
+        ):
+            print("\nProcessing", input_file.name)
+            img_data = tifffile.imread(input_file)
+            stack_handler.add_image(img_data, i)
+        filename = (
+            args.output_name
+            + f"_stack{stack_idx*stack_number:04d}"
+            + f"_{(stack_idx+1)*stack_number:04d}"
+        )
+        output_file = output_dir / filename
+        tifffile.imwrite(
+            str(output_file) + ".tif", stack_handler.export_stack()
+        )
 
 else:
-    assert tifffile.imread(files[0]).shape[0] % args.num_acquisitions == 0
+    # Unstacking procedure
+    assert tifffile.imread(files[0]).shape[0] % args.z_slices == 0
     for i, input_file in enumerate(files):
         print("\nProcessing", input_file.name)
         img_data = tifffile.imread(input_file)
-        for j in range(img_data.shape[0] // args.num_acquisitions):
+        for j in range(img_data.shape[0] // args.z_slices):
             output_file = output_dir / f"{j:05d}_{input_file.name}"
-            output_data = img_data[
-                j * args.num_acquisitions : (j + 1) * args.num_acquisitions
-            ]
-            if args.num_acquisitions != 1:
-                # Adds a single channel (reconstructions)
+            output_data = img_data[j * args.z_slices : (j + 1) * args.z_slices]
+            if args.z_slices != 1:
+                # Prepends a single channel
                 output_data = output_data.reshape((1, *output_data.shape))
             tifffile.imwrite(str(output_file), output_data)
--- src/rcan/data_processing.py
+++ src/rcan/data_processing.py
@@ -8,6 +8,30 @@ import numpy as np
 
 
 def crop_volume(volume, num_steps, start, step, label):
+    """!
+    @brief Takes an image volume and divides part of it into smaller volumes
+    by cropping lateral sections (the full z dimension is used).
+
+    @param volume (np.ndarray) - image volume to crop
+    @param num_steps (tuple[int]) - number of images in each lateral dimension
+    (total number of subvolumes is the product)
+    @param start (tuple[int]) - start coordinates for crop region
+    @param step (tuple[int]) - lateral size of subvolume images
+    @param label (str) - prefix for output file names
+
+    @returns generator that yields image subvolumes
+    """
+    for dim in range(2):
+        crop_boundaries = [
+            0,
+            start[dim],
+            start[dim] + num_steps[dim] * step[dim],
+            volume.shape[1 + dim],
+        ]
+        if crop_boundaries != sorted(crop_boundaries):
+            raise ValueError(
+                f"Cropping layout out of bounds in lateral dimension {dim}."
+            )
     for i in range(num_steps[0]):
         for j in range(num_steps[1]):
             subvolume = volume[
@@ -29,6 +53,12 @@ def conv_omx_to_czxy(original, n_phases, n_angles):
 
     @returns np.ndarray Converted image array
     """
+    if original.shape[0] % (n_phases * n_angles) != 0:
+        raise ValueError(
+            "number of acquisitions should divide channel dimension length."
+        )
+    if len(original.shape) != 3:
+        raise ValueError("omx image should have shape length 3.")
     converted = np.zeros(
         (
             n_phases * n_angles,
@@ -57,7 +87,10 @@ def conv_czxy_to_omx(original, n_phases, n_angles):
 
     @returns np.ndarray Converted image array
     """
-    assert original.shape[0] == n_phases * n_angles
+    if original.shape[0] != n_phases * n_angles:
+        raise ValueError("channel dimension should equal n_phases x n_angles.")
+    if len(original.shape) != 4:
+        raise ValueError("czxy image should have shape length 4.")
     converted = np.zeros(
         (
             original.shape[0] * original.shape[1],
@@ -85,7 +118,12 @@ def conv_omx_to_paz(original, n_phases, n_angles):
 
     @returns np.ndarray Converted image array
     """
-    assert len(original.shape) == 3
+    if original.shape[0] % (n_phases * n_angles) != 0:
+        raise ValueError(
+            "number of acquisitions should divide channel dimension length."
+        )
+    if len(original.shape) != 3:
+        raise ValueError("omx image should have shape length 3.")
     converted = np.zeros_like(original)
     for z in range(original.shape[0] // (n_phases * n_angles)):
         for a in range(n_angles):
@@ -113,7 +151,12 @@ def conv_paz_to_omx(original, n_phases, n_angles):
 
     @returns np.ndarray Converted image array
     """
-    assert len(original.shape) == 3
+    if original.shape[0] % (n_phases * n_angles) != 0:
+        raise ValueError(
+            "number of acquisitions should divide channel dimension length."
+        )
+    if len(original.shape) != 3:
+        raise ValueError("omx image should have shape length 3.")
     converted = np.zeros_like(original)
     for z in range(original.shape[0] // (n_phases * n_angles)):
         for a in range(n_angles):
@@ -129,3 +172,81 @@ def conv_paz_to_omx(original, n_phases, n_angles):
                 ...,
             ]
     return converted
+
+
+class ImageStack:
+    """!
+    @brief Handles creation and loading of image hyperstacks in order to make
+    reconstructions using ImageJ easier.
+    """
+
+    def __init__(self, dim, stack_number, stack_idx, sample, files, n_acq):
+        """!
+        @brief Initialises class.
+
+        @param dim (int) - Dimension of images
+        @param stack_number (int) - Number of images in the stack
+        @param stack_idx (int) - The index of the stack within the set of
+        stacks for the files list
+        @param sample (np.ndarray) - Image from the directory which enables
+        correct image stack shape/dtype and error catching
+        @param files (list) - List of all files in directory
+        @param n_acq (int) - Number of SIM acquisitions in the images
+        """
+        self.dim = dim
+        self.n_acq = n_acq
+        self.sample = sample
+        # Accounts for remainder of files at the end that cannot make a
+        # full stack
+        stack_number_adjusted = len(
+            files[stack_idx * stack_number : (stack_idx + 1) * stack_number]
+        )
+        if dim == 2:
+            stack = np.zeros(
+                (
+                    stack_number_adjusted,
+                    *sample.shape,
+                )
+            ).astype(sample.dtype)
+            self.stack = stack
+
+        elif dim == 3:
+            self.n_z = sample.shape[0] // n_acq
+            stack = np.zeros(
+                (
+                    stack_number_adjusted * self.n_z,
+                    n_acq,
+                    *sample.shape[1:],
+                )
+            ).astype(sample.dtype)
+            self.stack = stack
+
+        else:
+            raise ValueError("dim must be 2 or 3.")
+
+    def add_image(self, img_data, i):
+        """!
+        @brief Adds an image to the initialised stack
+
+        @param img_data (np.ndarray) - Image to be added
+        @param i (int) - Index of the image in the stack
+        """
+        if img_data.shape != self.sample.shape:
+            raise ValueError("all images must be the same shape.")
+        elif img_data.dtype != self.sample.dtype:
+            raise ValueError("all images must have the same data type.")
+        if self.dim == 2:
+            self.stack[i, ...] = img_data
+        else:
+            for z in range(self.n_z):
+                self.stack[i * self.n_z + z, ...] = img_data[
+                    z * self.n_acq : (z + 1) * self.n_acq, ...
+                ]
+
+    def export_stack(self):
+        """!
+        @brief Returns the stack.
+
+        @returns np.ndarray
+        """
+        return self.stack
